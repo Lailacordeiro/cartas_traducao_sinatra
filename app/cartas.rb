152362a5ea89f015f081d89108bad6000dda48b5
Campus Code 07 <campuscode07@Campuss-Mac-mini-7.local>
2020-03-06 22:14:22 -0300

    Aula de ruby

--- app/cartas.rb
+++ app/cartas.rb
@@ -5,4 +5,11 @@ set :static, true
 Tilt.register Tilt::ERBTemplate, 'html.erb'
 
 get '/' do
+  @cartas = []
+  @cartas << Carta.new(portugues:'Vermelho', ingles: 'Red')
+  @cartas << Carta.new(portugues:'Amarelo', ingles: 'Yellow')
+  @cartas << Carta.new(portugues:'Azul', ingles: 'Blue')
+  @cartas << Carta.new(portugues:'Amor', ingles: 'Love')
+  @cartas << Carta.new(portugues:'Carro', ingles: 'Car')
+  erb :index
 end
